2f8ba898d50f6f5cac39efdb1c8d6cbbcc004e8d
Lyn <lynfinman@gmail.com>
2022-08-11 15:37:27 -0400

    styled hero landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+// // client/src/components/App.js
+// import React from 'react';
+// import GlobalStyle from './globalStyles';
+// import { useState, useEffect } from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     fetch('/hello')
+//       .then((r) => r.json())
+//       .then((data) => setCount(data.count));
+//   }, []);
+
+//   return (
+//     <BrowserRouter>
+//       <GlobalStyle />
+//       <Switch>
+//         <Route path='/' exact component={Home} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 // client/src/components/App.js
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import Home from './pages/Home';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -19,7 +45,7 @@ function App() {
             <h1>Test Route</h1>
           </Route>
           <Route path='/'>
-            <h1>Page Count: {count}</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
