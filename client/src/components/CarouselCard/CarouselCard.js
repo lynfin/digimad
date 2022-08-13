b0b6c13b3ae67bd057d4270e16428cb8742349c6
Lyn <lynfinman@gmail.com>
2022-08-13 11:27:34 -0400

    created separate carouselcard

---
+++ client/src/components/CarouselCard/CarouselCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextWrapper } from '../../globalStyles';
+import { ImageWrapper, CarouselImage, CardButton } from './CarouselCardStyles';
+
+function CarouselCard({ el, index }) {
+  return (
+    <ImageWrapper key={index}>
+      <CarouselImage src={el.image} />
+      <TextWrapper size='1.1rem' margin='0.4rem 0 0' weight='bold'>
+        {el.name}
+      </TextWrapper>
+      <TextWrapper size='0.9rem' margin='0.7rem' color='#4f4f4f'>
+        {el.category}
+      </TextWrapper>
+      <CardButton>Details</CardButton>
+    </ImageWrapper>
+  );
+}
+
+export default CarouselCard;
