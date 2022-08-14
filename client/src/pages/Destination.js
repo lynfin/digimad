834c4aa097b55c4826adfb5a050e8beeaf964099
Lyn <lynfinman@gmail.com>
2022-08-14 07:30:03 -0400

    added full parameter to destination show

--- client/src/pages/Destination.js
+++ client/src/pages/Destination.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Container, MainHeading, TextWrapper } from '../globalStyles';
 import SpeedSummary from '../components/SpeedSummary/SpeedSummary';
 import ErrorPage from '../components/ErrorPage/ErrorPage';
@@ -7,8 +8,22 @@ import { useLocation } from 'react-router-dom';
 function Destination({ user, selectedDestination }) {
   const location = useLocation();
   const state = location.state;
-  console.log(user);
-  console.log(state);
+  const [destinationDetails, setDestinationDetails] = useState(null);
+
+  useEffect(() => {
+    fetch(`/destinations/${state.el.id}?full`).then((res) => {
+      if (res.ok) {
+        res.json().then(setDestinationDetails);
+      } else {
+        res.json().then((data) => console.log(data.error));
+      }
+    });
+  }, [state.el]);
+  console.log('IN DESTINATION: DETAILS ARE:');
+  console.log(destinationDetails);
+  // Initially had trouble setting selectedDestination at App level
+  // so used state with Link to pass in the destination overview.
+  // Might be able to get rid of state.el and just use selectedDestination now.
   return (
     <>
       {state && state.el ? (
