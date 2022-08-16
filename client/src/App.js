31ae4b88d7a8e2871fe587f2b3a4e6b79d2066f9
Lyn <lynfinman@gmail.com>
2022-08-16 12:03:28 -0400

    removed destination_sets after refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ import { parseISO, format } from 'date-fns';
 
 function App() {
   const [destinations, setDestinations] = useState([]);
-  const [destination_sets, setDestinationSets] = useState([]);
+  //const [destination_sets, setDestinationSets] = useState([]);
   const [errors, setErrors] = useState(false);
   const [user, setUser] = useState(null);
-  const [selectedDestinationId, setSelectedDestinationId] = useState(null);
+  //const [selectedDestinationId, setSelectedDestinationId] = useState(null);
   const [selectedDestination, setSelectedDestination] = useState(null);
 
   useEffect(() => {
@@ -35,79 +35,79 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    // TODO: create basic map, then add the more detailed info?
-    // Build once with all the info, then rename appropriate entry to summary1/summary2
-    let wifi_sort = destinations
-      .sort((a, b) => (a['maximum_wifi'] > b['maximum_wifi'] ? -1 : 1))
-      .map((d) => {
-        return {
-          header: d.name,
-          location: `${d.address.city}, ${d.address.country}`,
-          summary1: `${d.maximum_wifi} Max Mbps`,
-          summary2: `${d.total_tests} tests`,
-          image: d.image,
-          id: d.id,
-        };
-      });
-    let rating_sort = destinations
-      .sort((a, b) => (a.average_tech_rating > b.average_tech_rating ? -1 : 1))
-      .map((d) => {
-        return {
-          header: d.name,
-          location: `${d.address.city}, ${d.address.country}`,
-          summary1: `${parseFloat(d.average_tech_rating).toFixed(
-            2
-          )} Average Rating`,
-          summary2: `${d.total_visits} visits`,
-          image: d.image,
-          id: d.id,
-        };
-      });
-    let cell_coverage = destinations
-      .sort((a, b) =>
-        a.provider_fastest_cellular_download[1] >
-        b.provider_fastest_cellular_download[1]
-          ? -1
-          : 1
-      )
-      .map((d) => {
-        return {
-          header: d.name,
-          location: `${d.address.city}, ${d.address.country}`,
-          summary1: `${parseFloat(
-            d.provider_fastest_cellular_download[1]
-          ).toFixed(2)} Mbps`,
-          summary2: d.provider_fastest_cellular_download[0],
-          image: d.image,
-          id: d.id,
-        };
-      });
-    let most_recent_test = destinations
-      .sort((a, b) => (a.most_recent_test > b.most_recent_test ? -1 : 1))
-      .map((d) => {
-        return {
-          header: d.name,
-          location: `${d.address.city}, ${d.address.country}`,
-          summary1: format(parseISO(d.most_recent_test), 'MM/dd/yyyy'),
-          summary2: `${d.total_tests} tests`,
-          image: d.image,
-          id: d.id,
-        };
-      });
-    let arraryOfDestinations = [
-      { data: wifi_sort, title: 'Best Wifi' },
-      { data: cell_coverage, title: 'Best Cell Coverage' },
-      { data: rating_sort, title: 'Top Rated' },
-      { data: most_recent_test, title: 'Recently Visited' },
-    ];
-    setDestinationSets(arraryOfDestinations);
-  }, [destinations]);
+  // useEffect(() => {
+  //   // TODO: create basic map, then add the more detailed info?
+  //   // Build once with all the info, then rename appropriate entry to summary1/summary2
+  //   let wifi_sort = destinations
+  //     .sort((a, b) => (a['maximum_wifi'] > b['maximum_wifi'] ? -1 : 1))
+  //     .map((d) => {
+  //       return {
+  //         header: d.name,
+  //         location: `${d.address.city}, ${d.address.country}`,
+  //         summary1: `${d.maximum_wifi} Max Mbps`,
+  //         summary2: `${d.total_tests} tests`,
+  //         image: d.image,
+  //         id: d.id,
+  //       };
+  //     });
+  //   let rating_sort = destinations
+  //     .sort((a, b) => (a.average_tech_rating > b.average_tech_rating ? -1 : 1))
+  //     .map((d) => {
+  //       return {
+  //         header: d.name,
+  //         location: `${d.address.city}, ${d.address.country}`,
+  //         summary1: `${parseFloat(d.average_tech_rating).toFixed(
+  //           2
+  //         )} Average Rating`,
+  //         summary2: `${d.total_visits} visits`,
+  //         image: d.image,
+  //         id: d.id,
+  //       };
+  //     });
+  //   let cell_coverage = destinations
+  //     .sort((a, b) =>
+  //       a.provider_fastest_cellular_download[1] >
+  //       b.provider_fastest_cellular_download[1]
+  //         ? -1
+  //         : 1
+  //     )
+  //     .map((d) => {
+  //       return {
+  //         header: d.name,
+  //         location: `${d.address.city}, ${d.address.country}`,
+  //         summary1: `${parseFloat(
+  //           d.provider_fastest_cellular_download[1]
+  //         ).toFixed(2)} Mbps`,
+  //         summary2: d.provider_fastest_cellular_download[0],
+  //         image: d.image,
+  //         id: d.id,
+  //       };
+  //     });
+  //   let most_recent_test = destinations
+  //     .sort((a, b) => (a.most_recent_test > b.most_recent_test ? -1 : 1))
+  //     .map((d) => {
+  //       return {
+  //         header: d.name,
+  //         location: `${d.address.city}, ${d.address.country}`,
+  //         summary1: format(parseISO(d.most_recent_test), 'MM/dd/yyyy'),
+  //         summary2: `${d.total_tests} tests`,
+  //         image: d.image,
+  //         id: d.id,
+  //       };
+  //     });
+  //   let arraryOfDestinations = [
+  //     { data: wifi_sort, title: 'Best Wifi' },
+  //     { data: cell_coverage, title: 'Best Cell Coverage' },
+  //     { data: rating_sort, title: 'Top Rated' },
+  //     { data: most_recent_test, title: 'Recently Visited' },
+  //   ];
+  //   setDestinationSets(arraryOfDestinations);
+  // }, [destinations]);
 
   if (errors) return <h1>{errors}</h1>;
 
   function handleDestinationSelected(selectedId) {
-    setSelectedDestinationId(selectedId);
+    //setSelectedDestinationId(selectedId);
     setSelectedDestination(
       destinations.find((d) => {
         return d.id === selectedId;
@@ -125,7 +125,6 @@ function App() {
         </Route>
         <Route exact path='/'>
           <Home
-            destination_sets={destination_sets}
             user={user}
             onDestinationSelected={handleDestinationSelected}
             destinations={destinations}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Carousel from '../components/Carousel/Carousel';
 
-function Home({ destination_sets, user, onDestinationSelected, destinations }) {
+function Home({ user, onDestinationSelected, destinations }) {
   const destinationCardStyles = [
     {
       data: {
