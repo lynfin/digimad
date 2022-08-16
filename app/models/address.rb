07e11ec18364bd4d835feb5d59a2e5f07e663d81
Lyn <lynfinman@gmail.com>
2022-08-16 18:38:25 -0400

    added react-flagkit for dropdown use

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,6 @@
 class Address < ApplicationRecord
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
-  has_one :destination
   # may want to look into more complex validation, or using Geocoder Gem to generate lat/long
   # https://stackoverflow.com/questions/35978951/validating-longitude-latitude-with-geocoder-gem
 end
