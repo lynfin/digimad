b6e11e1766c193044fc50f3c7531bceb1b92be2f
Lyn <lynfinman@gmail.com>
2022-08-16 20:02:44 -0400

    first pass at dropdown with flags

--- client/src/components/Filters/Filters.js
+++ client/src/components/Filters/Filters.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Heading, TextWrapper } from '../../globalStyles';
+import React, { useState } from 'react';
+import {
+  Heading,
+  TextWrapper,
+  ContainerDEFAULT,
+  Header,
+  RowDEFAULT,
+} from '../../globalStyles';
 import { FiltersSection, FiltersWrapper } from './FiltersStyles';
+import Dropdown from '../Dropdown/Dropdown';
+
+// <FiltersSection id='FiltersSection'>
+// <FiltersWrapper>
+//   <Heading>Filters</Heading>
+//   <RowDEFAULT justify='center' align='center' mt='4rem'>
+//     <Dropdown country={country} setCountry={setCountry} />
+//   </RowDEFAULT>
+// </FiltersWrapper>
+// </FiltersSection>
 
 function Filters({}) {
+  const [currency, setCurrency] = useState('USD');
   return (
-    <FiltersSection id='LoadingPage'>
+    <FiltersSection>
       <FiltersWrapper>
-        <Heading>Filters</Heading>
-        <TextWrapper
-          mb='1.4rem'
-          weight='600'
-          size='1.1rem'
-          color='white'
-          align='center'
-        >
-          filter here
-        </TextWrapper>
+        <ContainerDEFAULT>
+          <Header color='red'>Filters</Header>
+          <RowDEFAULT justify='center' align='center' mt='4rem'>
+            <Dropdown currency={currency} setCurrency={setCurrency} />
+          </RowDEFAULT>
+        </ContainerDEFAULT>
       </FiltersWrapper>
     </FiltersSection>
   );
--- client/src/globalStyles.js
+++ client/src/globalStyles.js
@@ -7,6 +7,9 @@ const GlobalStyle = createGlobalStyle`
   padding: 0;
   font-family: 'Montserrat', sans-serif;
   }
+  body {
+  background-color: #ffe357;
+}
 `;
 
 export const Container = styled.div`
@@ -95,6 +98,52 @@ const DEFAULT_DIV = styled.div`
   max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : '')};
   min-height: ${({ minHeight }) => (minHeight ? minHeight : '')};
   background-color: ${({ background }) => (background ? background : '')};
+`;
+
+export const ContainerDEFAULT = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin: ${({ margin }) => (margin ? margin : '0 auto')};
+  padding: ${({ padding }) => (padding ? padding : '0 15px')};
+`;
+
+export const RowDEFAULT = styled(DEFAULT_DIV)`
+  display: flex;
+  justify-content: ${({ justify }) => (justify ? justify : '')};
+  align-items: ${({ align }) => (align ? align : '')};
+  gap: ${({ gap }) => (gap ? gap : '')};
+`;
+
+export const ColumnDEFAULT = styled(DEFAULT_DIV)`
+  display: flex;
+  flex-flow: column;
+  justify-content: ${({ justify }) => (justify ? justify : '')};
+  align-items: ${({ align }) => (align ? align : '')};
+  gap: ${({ gap }) => (gap ? gap : '')};
+`;
+
+export const Header = styled.h1`
+  color: ${({ color }) => (color ? color : 'blue')};
+  text-align: center;
+  margin: 2rem;
+`;
+
+export const Text = styled.span`
+  font-size: ${({ size }) => (size ? size : '')};
+  font-weight: ${({ weight }) => (weight ? weight : '')};
+  letter-spacing: ${({ spacing }) => (spacing ? spacing : '')};
+  text-align: ${({ align }) => (align ? align : '')};
+  padding: ${({ padding }) => (padding ? padding : '')};
+  margin: ${({ margin }) => (margin ? margin : '')};
+  margin-bottom: ${({ mb }) => (mb ? mb : '')};
+  margin-top: ${({ mt }) => (mt ? mt : '')};
+  width: ${({ width }) => (width ? width : 'auto')};
+  min-width: ${({ minWidth }) => (minWidth ? minWidth : '')};
+  max-width: ${({ maxWidth }) => (maxWidth ? maxWidth : '')};
+  height: ${({ height }) => (height ? height : '')};
+  max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : '')};
+  min-height: ${({ minHeight }) => (minHeight ? minHeight : '')};
+  color: ${({ color }) => (color ? color : '')};
 `;
 
 export const Row = styled.div`
